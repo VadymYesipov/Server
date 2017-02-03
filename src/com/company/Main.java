66167eca5795bf7bec3e8d1d0f8c805e3e9117b3
User <User>
2017-02-03 16:45:44 +0200

    eto moi commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,6 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         Server server = new Server();
-        //khabib zatashit
+        //eto master
     }
 }
