95b4f52a779a9bbc151f238a52ffb98d901c2461
User <User>
2017-02-03 15:45:28 +0200

    my first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Server server = new Server();
+    }
+}
---
+++ src/com/company/Server.java
@@ -0,0 +1,60 @@
+package com.company;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+/**
+ * Created by User on 003 03.02.17.
+ */
+public class Server {
+    private final int port = 8888;
+    ServerSocket serverSocket;
+    ArrayList<Socket> sockets;
+    DataInputStream[] dataInputStreams;
+    DataOutputStream[] dataOutputStream;
+
+    public ServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+    public void setServerSocket(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public Server() throws IOException {
+        serverSocket = new ServerSocket(port);
+        sockets = new ArrayList<Socket>();
+        while (true) {
+            sockets.add(serverSocket.accept());
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        int k = 0;
+                        for (Socket socket : sockets) {
+                            try {
+                                if (socket.getInputStream().available() > 0) {
+                                    k = socket.getInputStream().read();
+                                }
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        for (Socket socket :
+                                sockets) {
+                            try {
+                                socket.getOutputStream().write(k);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            }).start();
+        }
+    }
+}
