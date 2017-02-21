720466b6a2f5d059dfbb2135b4440fb88e5acf21
User <User>
2017-02-21 02:23:25 +0200

    Server without connection exception

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,15 +2,23 @@ package com.company;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.UnknownHostException;
+import java.net.*;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        Server server = new Server();
-        //eto master
+    public static void main(String[] args) {
+        try {
+            workServer();
+        } catch (SocketException e) {
+            System.out.println("her");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    private static void workServer()throws IOException {
+        Server server = new Server();
+        server.work();
+    }
+
 }
--- src/com/company/Server.java
+++ src/com/company/Server.java
@@ -1,21 +1,24 @@
 package com.company;
 
+import javafx.application.Application;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Created by User on 003 03.02.17.
  */
 public class Server {
-    private final int port = 8888;
+    private final int PORT = 8887;
     ServerSocket serverSocket;
-    ArrayList<Socket> sockets;
-    DataInputStream[] dataInputStreams;
-    DataOutputStream[] dataOutputStream;
+    volatile ArrayList<Socket> sockets;
+    boolean flag;
+    int count;
 
     public ServerSocket getServerSocket() {
         return serverSocket;
@@ -26,42 +29,84 @@ public class Server {
     }
 
     public Server() throws IOException {
-        serverSocket = new ServerSocket(port);
+        serverSocket = new ServerSocket(PORT);
         sockets = new ArrayList<Socket>();
+    }
 
-        while (true) {
-            sockets.add(serverSocket.accept());
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    while (true) {
-                        int k = 0;
-                        for (Socket socket : sockets) {
-                            try {
-                                while(socket.getInputStream().available()>0) {
-                                    k = socket.getInputStream().read();
-                                    for (Socket socket2 : sockets) {
-                                        try {
-                                            socket2.getOutputStream().write(k);
-                                        } catch (IOException e) {
-                                            e.printStackTrace();
-                                        }
-                                    }
-                                }
-                            } catch (IOException e) {
-                                e.printStackTrace();
+    protected void work() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName());
+                    acceptSocket();
+                }
+            }
+        }).start();
+    }
+
+    private void acceptSocket() {
+        try {
+            if (sockets.add(serverSocket.accept())) {
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (flag) {
+                            System.out.println(Thread.currentThread().getName());
+                            flag = false;
+                            //System.exit(0);
+                        }
+                        while (true) {
+                            if (flag) {
+                                break;
+                            } else if (count > 0) {
+                                Thread.currentThread().setName("Thread-" + count);
+                                System.out.println(Thread.currentThread().getName());
+                                listenAndSay();
+                            } else if (count == 0) {
+                                System.out.println(Thread.currentThread().getName());
+                                listenAndSay();
                             }
-                            /*for (Socket socket2 : sockets) {
-                                try {
-                                    socket2.getOutputStream().write(k);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                }
-                            }*/
                         }
                     }
-                }
-            }).start();
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void listenAndSay() {
+        byte[] buffer = new byte[255];
+        byte[] mode = new byte[255];
+        try {
+            listner(Integer.parseInt((Thread.currentThread().getName().substring(7))), buffer, mode);
+            speaker(buffer, mode);
+        } catch (Exception e) {
+            return;
+        }
+    }
+
+    private void listner(int number, byte[] buffer, byte[] mode) {
+        try {
+            sockets.get(number - 1).getInputStream().read(buffer);
+            sockets.get(number - 1).getInputStream().read(mode);
+        } catch (IOException e) {
+            sockets.remove(number - 1);
+            System.out.println(sockets.size());
+            flag = true;
+            count = number;
+        }
+    }
+
+    private synchronized void speaker(byte[] buffer, byte[] mode) {
+        for (int j = 0; j < sockets.size(); j++) {
+            try {
+                sockets.get(j).getOutputStream().write(buffer);
+                sockets.get(j).getOutputStream().write(mode);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
